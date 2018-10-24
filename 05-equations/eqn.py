79fc210767b6620149685dc6b9d1840e3c08584d
jurajvandor <433451@mail.muni.cz>
2018-10-24 02:16:17 +0200

    05 done

--- 05-equations/eqn.py
+++ 05-equations/eqn.py
@@ -1,7 +1,7 @@
 import numpy
 import re
 import sys
-
+import collections
 
 def parse():
     file = open(sys.argv[1])
@@ -18,7 +18,10 @@ def parse():
             res = re.match(r"([0-9]*)([a-zA-Z])", ex)
             if res is None:
                 continue
-            coefficient = int(res.group(1))
+            if res.group(1) != "":
+                coefficient = int(res.group(1))
+            else:
+                coefficient = 1
             variable = res.group(2)
             if i != 0 and split[i-1] == "-":
                 coefficient *= -1
@@ -36,10 +39,23 @@ def parse():
     return list_of_lists, variables, other_side_of_eq
 
 
+def compute(parsed_lists):
+    if len(parsed_lists[1]) != len(parsed_lists[2]):
+        print("solution space dimension: " + str(abs(len(parsed_lists[1]) - len(parsed_lists[2]))))
+        return
+    matrix = numpy.array(parsed_lists[0])
+    list_of_constants = numpy.array(parsed_lists[2])
+    rank_matrix = numpy.linalg.matrix_rank(matrix)
+    augumented_matrix = numpy.hstack((matrix, numpy.expand_dims(list_of_constants, axis=1)))
+    rank_augumented_matrix = numpy.linalg.matrix_rank(augumented_matrix)
+    if rank_augumented_matrix != rank_matrix:
+        print("no solution")
+        return
+    result = numpy.linalg.solve(matrix, list_of_constants)
+    d = dict(zip(parsed_lists[1], result))
+    ordered_dict = collections.OrderedDict(sorted(d.items()))
+    for variable, value in ordered_dict.items():
+        print(variable + " = " + str(value))
 
-result = parse()
 
-print(result[0])
-print(result[1])
-print(result[2])
-
+compute(parse())
