c07f658c49a1fa87c41517908dfc7d4c95ac564d
jurajvandor <433451@mail.muni.cz>
2018-11-13 22:43:30 +0100

    08 stat.py done

--- 08-csv/stat.py
+++ 08-csv/stat.py
@@ -1,45 +1,49 @@
 import pandas
 import sys
+import json
 
 
-def parse():
-    csv = pandas.read_csv(sys.argv[1])
+def column_name(column):
     if sys.argv[2] == "dates":
-        dic = dates(csv)
-    if sys.argv[2] == "deadlines":
-        dic = deadlines(csv)
+        return column[:-3]
     if sys.argv[2] == "exercises":
-        dic = exercises(csv)
+        return column[-2:]
 
 
-def dates(csv):
+def transform_dataframe(csv):
     dict_of_dates = dict()
-    #for item in csv:
-        #if item == "student":
-            #continue
-        #else:
-            #dict_of_dates[item[:-3]] = pandas.DataFrame()
     for column in csv:
         if column == "student":
             continue
-        #for row in csv[column]:
-        #    dict_of_dates[column[:-3]] = dict_of_dates[column[:-3]].append({column[:-3]: row}, ignore_index=True)
-        added = pandas.concat([pandas.DataFrame([row], columns=[column[:-3]]) for row in csv[column]], ignore_index=True)
-        if column[:-3] not in dict_of_dates.keys():
-            dict_of_dates[column[:-3]] = added
+        if column_name(column) not in dict_of_dates.keys():
+            dict_of_dates[column_name(column)] = csv[column]
         else:
-            dict_of_dates[column[:-3]] = pandas.concat([dict_of_dates[column[:-3]], added], ignore_index=True)
-    data_frame = pandas.concat(dict_of_dates.values(), axis=1)
-    data_frame = data_frame
-    print(data_frame)
+            dict_of_dates[column_name(column)] = dict_of_dates.get(column_name(column)) + csv[column]
+    data_frame = pandas.concat(dict_of_dates, axis=1)
+    return data_frame
 
 
-def deadlines(csv):
-    pass
+def print_data(data_frame):
+    mean = data_frame.mean()
+    quantile_75 = data_frame.quantile(0.75)
+    quantile_25 = data_frame.quantile(0.25)
+    median = data_frame.quantile()
+    non_zero_count = data_frame.astype(bool).sum(axis=0)
+    d = dict()
+    for i in mean.keys():
+        column = dict()
+        d[i] = column
+        column["mean"] = mean[i]
+        column["median"] = median[i]
+        column["first"] = quantile_25[i]
+        column["last"] = quantile_75[i]
+        column["passed"] = int(non_zero_count[i])
+    json.dump(d, sys.stdout, indent=4)
 
 
-def exercises(csv):
-    pass
-
-
-parse()
+csv = pandas.read_csv(sys.argv[1])
+if sys.argv[2] == "deadlines":
+    csv = csv.drop("student", axis=1)
+    print_data(csv)
+else:
+    print_data(transform_dataframe(csv))
