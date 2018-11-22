8a6ded514479f1a3c3979978c4477a06aca21081
jurajvandor <433451@mail.muni.cz>
2018-11-22 13:08:59 +0100

    09 working

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -4,13 +4,12 @@ import sys
 import json
 
 
-def is_json(myjson):
+def is_json(maybe_json):
     try:
-        json_object = json.loads(myjson)
+        json.loads(maybe_json)
     except ValueError:
         return False
     return True
-
 
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
@@ -18,41 +17,42 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         client = http.client.HTTPConnection(str(sys.argv[2]), timeout=1)
         headers = {"Content-type": "application/json", "Accept": "application/json"}
         client.request("GET", "/", None, headers)
-        res = client.getresponse()
-        self.send_response(200)
-        json_dict = dict()
-        json_dict["code"] = res.getcode()
-        body = str(res.read())
-        json_dict["headers"] = res.getheaders()
-        if res.getheader("Content-type")[0:17] == "application/json" and is_json(body):
-            json_dict["json"] = body
-        else:
-            json_dict["content"] = body
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        self.wfile.write(bytes(json.dumps(json_dict).encode("UTF8")))
-
+        self.create_json(client.getresponse())
 
     def do_POST(self):
         length = int(self.headers.get('content-length', 0))
         data = self.rfile.read(length)
-        loaded_json = json.loads(data)
-        client = http.client.HTTPConnection(loaded_json["url"], timeout=loaded_json["timeout"])
-        body = loaded_json["content"] if loaded_json["type"] == "POST" else None
-        client.request(loaded_json["type"], "/", body, loaded_json["headers"])
-        res = client.getresponse()
+        try:
+            loaded_json = json.loads(data)
+        except ValueError:
+            self.send_json({"code": "invalid json"})
+            return
+        if "url" not in loaded_json.keys() or ("type" in loaded_json.keys() and "content" not in loaded_json.keys() and loaded_json["type"] == "POST"):
+            self.send_json({"code": "invalid json"})
+            return
+        timeout = loaded_json["timeout"] if "timeout" in loaded_json.keys() else 1
+        client = http.client.HTTPConnection(loaded_json["url"], timeout=timeout)
+        body = loaded_json["content"] if "type" in loaded_json.keys() and loaded_json["type"] == "POST" else None
+        headers = loaded_json["headers"] if "headers" in loaded_json.keys() else dict()
+        client.request(loaded_json["type"], "/", body, headers)
+        self.create_json(client.getresponse())
+
+    def create_json(self, response):
+        self.send_response(200)
         json_dict = dict()
-        json_dict["code"] = res.getcode()
-        body = str(res.read())
-        json_dict["headers"] = res.getheaders()
-        if res.getheader("Content-type")[0:17] == "application/json" and is_json(body):
+        json_dict["code"] = response.getcode()
+        body = str(response.read())
+        json_dict["headers"] = response.getheaders()
+        if response.getheader("Content-type")[0:17] == "application/json" and is_json(body):
             json_dict["json"] = body
         else:
             json_dict["content"] = body
+        self.send_json(json_dict)
+
+    def send_json(self, dict):
         self.send_header("Content-Type", "application/json")
-        self.wfile.write(bytes(json.dumps(json_dict).encode("UTF8")))
-        self.send_response(200)
         self.end_headers()
+        self.wfile.write(bytes(json.dumps(dict).encode()))
 
 
 port = int(sys.argv[1])
