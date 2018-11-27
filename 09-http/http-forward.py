9686b19f9dc5e1e350413d57a7ac462dbdb2b99c
jurajvandor <433451@mail.muni.cz>
2018-11-27 23:59:07 +0100

    09 fix...

--- 09-http/http-forward.py
+++ 09-http/http-forward.py
@@ -49,7 +49,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             return
         timeout = loaded_json["timeout"] if "timeout" in loaded_json.keys() else 1
         body = loaded_json["content"] if "type" in loaded_json.keys() and loaded_json["type"] == "POST" else None
-        if not isinstance(body, str):
+        if body is not None and not isinstance(body, str):
             body = json.dumps(body)
         headers = loaded_json["headers"] if "headers" in loaded_json.keys() else dict()
         parsed_url = url_parse(loaded_json["url"])
